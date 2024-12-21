5499cb0ecc72b72f7f022e837c1e64f36b083869
Ethan Minja <79723001+Ethan-Bit-256@users.noreply.github.com>
2024-12-21 20:40:56 +0300

    Fill
    Finally made the Text area fill the window

--- KonEdit/Editor.py
+++ KonEdit/Editor.py
@@ -2,7 +2,7 @@ from MainWindow import *
 
 """ Where the actual text editing will take place """
 EditArea = Text(MainWindow, yscrollcommand = Scroll.set)
-EditArea.pack(fill=BOTH)
+EditArea.pack(expand=True, fill=BOTH)
 
 """ Making the scrollbar work with the EditArea """
 Scroll.config(command=EditArea.yview)
