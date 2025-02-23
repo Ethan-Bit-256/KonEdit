1224a3b3ef7acfafa2314b435c8c9f934b9630e2
Ethan Minja <79723001+Ethan-Bit-256@users.noreply.github.com>
2025-02-23 19:43:55 +0300

    There was a syntax error

--- KonEdit/About.py
+++ KonEdit/About.py
@@ -8,6 +8,7 @@ def AboutWindow():
     About.wm_title("About KonEdit")
     About.resizable(False, False)
 
-""" The version text in the middle of it all """
+    """ The version text in the middle of it all """
     Version = Label(About, text="KonEdit 0.1.1")
     Version.pack(side="top", fill="both", expand=True, padx=100, pady=100)
+    
